ed82148cfdb9a4fe88f3911bfccfb21152d589d2
Hashem Khalifa <hashem.khalifa@gmail.com>
2019-05-16 23:14:58 +0200

    base structure for webpack js boilerplate

---
+++ src/index.js
@@ -0,0 +1,5 @@
+const app = document.getElementById('app');
+const headLine = document.createElement('h2');
+app.appendChild(headLine);
+
+headLine.innerText = 'Hello from Webpack JS Boilerplate';
---
+++ webpack/webpack.common.js
@@ -0,0 +1,61 @@
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ScriptExtHtmlWebpackPlugin = require('script-ext-html-webpack-plugin');
+const commonPaths = require('./paths');
+
+module.exports = {
+  entry: commonPaths.entryPath,
+  module: {
+    rules: [
+      {
+        enforce: 'pre',
+        test: /\.(js)$/,
+        loader: 'eslint-loader',
+        exclude: /(node_modules)/,
+        options: {
+          emitWarning: process.env.NODE_ENV !== 'production',
+        },
+      },
+      {
+        test: /\.(js)$/,
+        loader: 'babel-loader',
+        exclude: /(node_modules)/,
+      },
+      {
+        test: /\.(png|jpg|gif|svg)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              outputPath: commonPaths.imagesFolder,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(woff2|ttf|woff|eot)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              outputPath: commonPaths.fontsFolder,
+            },
+          },
+        ],
+      },
+    ],
+  },
+  resolve: {
+    modules: ['src', 'node_modules'],
+    extensions: ['*', '.js', '.jsx', '.css', '.scss'],
+  },
+  plugins: [
+    new webpack.ProgressPlugin(),
+    new HtmlWebpackPlugin({
+      template: commonPaths.templatePath,
+    }),
+    new ScriptExtHtmlWebpackPlugin({
+      defaultAttribute: 'async',
+    }),
+  ],
+};
